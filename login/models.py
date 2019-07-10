debbabbb8244796134b2f31b2d21b23b0188dabb
gns886 <1471176552@qq.com>
2019-07-10 14:06:26 +0800

    First Commit

--- login/models.py
+++ login/models.py
@@ -3,13 +3,6 @@ from defectManage.models import Module,Data
 # Create your models here.
 
 # Create your models here.
-class Users(models.Model):
-    username   = models.CharField(max_length=100)
-    password   = models.CharField(max_length=100)
-    authority  = models.IntegerField()
-    module     = models.ManyToManyField(Module, on_delete=models.CASCADE,null=True)
-    tel_number = models.CharField(max_length=20)
-    address    = models.CharField(max_length=100)
-    def __str__(self):
-        return self.username
-
+class Event:
+    def helloword(self):
+        return
